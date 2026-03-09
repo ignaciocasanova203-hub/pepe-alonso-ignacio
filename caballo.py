51eecfc99c6a2243f69e76d665ebff8860cc132b
Alonso <alonso.de@alumni.immune.institute>
2026-03-09 12:29:46 +0100

    Añadido rama main

--- caballo.py
+++
@@ -1,4 +0,0 @@
-class Caballo:
-    def __init__(self,blanca=True):
-        self.blanca=blanca
-        self.movimientos=[[1,2],[2,1]] #Esto en todas direcciones, es decir -1,2; -1,-2; 1,-2. Esto se hace en un for al hacer click y se resaltan aquellas casilla. 
